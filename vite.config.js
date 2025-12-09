d9500efb146b4e65169fb752224d95f23ce2604c
riships <avviare.rishi@gmail.com>
2025-12-09 23:46:30 +0530

    Fetch: Config File Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/rishi-portfolio',
+  base: '/rishi-portfolio/',
   build: {
     outDir: 'dist',
     sourcemap: false,
