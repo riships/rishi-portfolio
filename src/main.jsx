381ae883d3825d440e977706bed75b9ff38b4818
riships <avviare.rishi@gmail.com>
2025-12-10 00:19:06 +0530

    clean rebuild for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rishi-portfolio">
       <ThemeProvider>
         <App />
       </ThemeProvider>
