4f73e52065e44acaa8cd9909b88c368d6886357b
riships <avviare.rishi@gmail.com>
2025-12-10 00:17:13 +0530

    clean rebuild for gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/rishi-portfolio/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
 })
