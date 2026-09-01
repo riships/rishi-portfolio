675cfce004460cc4fcbe81c3c71d9994af9bb2e0
riships <avviare.rishi@gmail.com>
2026-09-01 10:35:31 +0530

    feat: add GitHub Actions deployment workflow, configure project base path, and integrate HashRouter for SPA hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css';
 import './assets/styles/theme.css';
 import './assets/styles/style.css';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
